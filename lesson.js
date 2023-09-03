113b64bb6b119aa0cca0ca571d0ffb8836ebe719
Andrew <andreytaburca@gmail.com>
2023-09-03 12:56:13 +0300

    starting of app

--- lesson.js
+++ lesson.js
@@ -1,1 +1,19 @@
-const a = 2*2;
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?","");
+let personalMovieDB = {
+count:numberOfFilms,
+movies:{},
+actors:{},
+genres:[],
+privat:false
+};
+      a = prompt("Один из последнихх просмотренных фильмов:",""),
+      b = +prompt("На сколько оцените фильм?",""),
+      c = prompt("Один из последнихх просмотренных фильмов",""),
+      d = +prompt("На сколько оцените фильм?","");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
+
+
+      
